c5f27b2bca331ce3a36f641cd2a137fa012549a0
CyberSpyderX <gpratik9259@gmail.com>
2023-03-26 23:29:44 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
-import cv2
-from streamlit_webrtc import webrtc_streamer
+import streamlit as st
 
-webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": "turn:relay.metered.ca:443", "username": "6c6333babd32db890da89835", "credential": "qN6FOiA4TSmFzDu3"}]})
+picture = st.camera_input("Take a picture")
+
+if picture:
+    st.image(picture)
