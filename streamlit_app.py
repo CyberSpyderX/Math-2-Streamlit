7bcfe551260323d1a185ce490ed3b6e71f765366
CyberSpyderX <gpratik9259@gmail.com>
2023-03-26 23:27:41 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import cv2
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": "stun:relay.metered.ca:80"}]})
+webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": "turn:relay.metered.ca:443", "username": "6c6333babd32db890da89835", "credential": "qN6FOiA4TSmFzDu3"}]})
