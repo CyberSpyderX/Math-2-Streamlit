81356923f6ed3035adfbc40a51f1ef62a1833c95
CyberSpyderX <gpratik9259@gmail.com>
2023-03-26 23:50:20 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import cv2
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration
 
-webrtc_streamer(key="example", rtc_configuration=RTCConfiguration({ "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}))
+webrtc_streamer(key="example", rtc_configuration=RTCConfiguration({ "iceServers": [{"urls": ["stun:stun1.l.google.com:19302"]}]}))
