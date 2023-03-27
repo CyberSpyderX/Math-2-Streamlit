9829c812d83ce050dd3681ff8aa44814b478407a
CyberSpyderX <gpratik9259@gmail.com>
2023-03-26 23:10:00 -0400

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,4 +20,4 @@ class VideoTransformer(VideoTransformerBase):
 
         return img
 
-webrtc_streamer(key="example", video_transformer_factory=VideoTransformer, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+webrtc_streamer(key="example", rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
